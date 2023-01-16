602f2b235bc959d0b451b92056186d6fdcd81ac3
Adebisi01 <oluwafemiadebisi01@gmail.com>
2023-01-16 18:34:53 +0100

    Removing files

--- E-commerce/src/App.tsx
+++ E-commerce/src/App.tsx
@@ -15,9 +15,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className=' text-purple-800'>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 4)}>
           count is {count}
         </button>
         <p>
